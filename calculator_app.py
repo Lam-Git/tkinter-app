6c6941951bc8ebba4ff414f450bbfc44ae8927d5
Lam Nguyen <lsnguyen0@gmail.com>
2021-02-28 11:59:33 -0800

    commit

--- calculator_app.py
+++ calculator_app.py
@@ -14,39 +14,63 @@ def button_click(number):
     e.insert(0, str(current) + str(number))
 
 
-# Add_function
-
-
 def button_clear():
     e.delete(0, END)
 
 
+# Add_function
 def button_add():
     first_number = e.get()
     global f_num  # global
+    global math
+    math = "addition"
     f_num = int(first_number)
     e.delete(0, END)
 
 
+# Equal button
 def button_equal():
     second_number = e.get()
     e.delete(0, END)
-    e.insert(0, f_num + int(second_number))  # from the global f_num +second_number
+
+    if math == "addition":
+        e.insert(0, f_num + int(second_number))  # from the global f_num +second_number
+    if math == "subtraction":
+        e.insert(0, f_num + int(second_number))
+    if math == "multiplication":
+        e.insert(0, f_num + int(second_number))
+    if math == "division":
+        e.insert(0, f_num + int(second_number))
 
 
 # Subtract_function
 def button_subtract():
-    return
+    first_number = e.get()
+    global f_num  # global
+    global math
+    math = "subtraction"
+    f_num = int(first_number)
+    e.delete(0, END)
 
 
 # Multiply_function
 def button_multiply():
-    return
+    first_number = e.get()
+    global f_num  # global
+    global math
+    math = "multiplication"
+    f_num = int(first_number)
+    e.delete(0, END)
 
 
 # Divide_function
 def button_divide():
-    return
+    first_number = e.get()
+    global f_num  # global
+    global math
+    math = "division"
+    f_num = int(first_number)
+    e.delete(0, END)
 
 
 # Define Buttons
