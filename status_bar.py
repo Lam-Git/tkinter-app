14c9e131f69045ab082f80d4dc650c5924421b64
Lam Nguyen <lsnguyen0@gmail.com>
2021-02-28 16:08:21 -0800

    commpt

--- status_bar.py
+++ status_bar.py
@@ -27,6 +27,9 @@ my_img5 = ImageTk.PhotoImage(resized4)
 
 image_list = [my_img1, my_img2, my_img3, my_img4, my_img5]
 
+status = Label(
+    root, text="Image 1 of " + str(len(image_list)), bd=1, relief=SUNKEN, anchor=E
+)
 
 my_label = Label(image=my_img1)
 my_label.grid(row=0, column=0, columnspan=3)
@@ -48,6 +51,15 @@ def forward(image_number):
     my_label.grid(row=0, column=0, columnspan=3)
     button_back.grid(row=1, column=0)
     button_forward.grid(row=1, column=2)
+
+    status = Label(
+        root,
+        text="Image " + str(image_number) + " of " + str(len(image_list)),
+        bd=1,
+        relief=SUNKEN,
+        anchor=E,
+    )
+    status.grid(row=2, column=0, columnspan=3, sticky=W + E)
 
 
 def back(image_number):
@@ -71,6 +83,17 @@ def back(image_number):
     button_back.grid(row=1, column=0)
     button_forward.grid(row=1, column=2)
 
+    # Update Status Bar
+    status = Label(
+        root,
+        text="Image " + str(image_number) + " of " + str(len(image_list)),
+        bd=1,
+        relief=SUNKEN,
+        anchor=E,
+    )
+
+    status.grid(row=2, column=0, columnspan=3, sticky=W + E)
+
 
 button_back = Button(root, text="<<", command=back, state=DISABLED)
 button_exit = Button(root, text="Exit Program", command=root.quit)
@@ -80,5 +103,6 @@ button_forward = Button(root, text=">>", command=lambda: forward(2))
 button_back.grid(row=1, column=0)
 button_exit.grid(row=1, column=1)
 button_forward.grid(row=1, column=2)
+status.grid(row=2, column=0, columnspan=3, sticky=W + E)
 
 root.mainloop()
